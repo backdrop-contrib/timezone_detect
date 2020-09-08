ac87fe11d01857caa825465791e7af66653dda68
laryn <laryn@cedc.org>
2020-09-08 13:57:38 -0500

    Result of Coder Upgrade

--- timezone_detect.js
+++ timezone_detect.js
@@ -2,23 +2,23 @@
  * @file
  * Determine and set user's timezone on page load.
  * 
- * @todo: Use Drupal.behaviors?
+ * @todo: Use Backdrop.behaviors?
  */
 jQuery(document).ready(function () {
 
   // Determine timezone from browser client using jsTimezoneDetect library.
   var tz = jstz.determine();
 
-  if (tz.name() != Drupal.settings.timezone_detect.current_timezone) {
+  if (tz.name() != Backdrop.settings.timezone_detect.current_timezone) {
 
     // Post timezone to callback url via ajax.
     jQuery.ajax({
       type: 'POST',
-      url: Drupal.settings.basePath + 'timezone-detect/ajax/set-timezone',
+      url: Backdrop.settings.basePath + 'timezone-detect/ajax/set-timezone',
       dataType: 'json',
       data: {
         timezone: tz.name(), 
-        token: Drupal.settings.timezone_detect.token
+        token: Backdrop.settings.timezone_detect.token
       }
     });
 
